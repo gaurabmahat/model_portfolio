bed7eb2867939a5f204df1ce2c6bf22bd78dcd69
gaurabmahat <91778163+gaurabmahat@users.noreply.github.com>
2026-08-20 11:25:29 +0300

    initial vite project. added routes to home page and gallery page. added inital design pic.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # model_portfolio
 A Portfolio designed to host Model's photos.
+
+## Design
+
+![Design](./model-portfolio/Design/Website-design.png)
+
+## Architecture
+
+![Architecture](./model-portfolio/Design/Website-architecture.png)
---
+++ model-portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import GalleryPage from './pages/GalleryPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/gallery' element={<GalleryPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
