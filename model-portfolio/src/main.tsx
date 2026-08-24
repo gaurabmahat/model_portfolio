a5aa1f7168633e00547a7b83aad9080ded9743fe
gaurabmahat <91778163+gaurabmahat@users.noreply.github.com>
2026-08-24 13:04:32 +0300

    added i18next config with english and finnish language support.

--- model-portfolio/src/main.tsx
+++ model-portfolio/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './theme/tokens.css'
 import './index.css'
 import App from './App.tsx'
+import './i18n.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
