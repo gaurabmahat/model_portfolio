5b50f2f65d200daf5a6e3249e4a887bbed207097
gaurabmahat <91778163+gaurabmahat@users.noreply.github.com>
2026-08-25 11:27:01 +0300

    updated gallery header to support Finnish language.

--- model-portfolio/src/components/GalleryHeader.tsx
+++ model-portfolio/src/components/GalleryHeader.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./GalleryHeader.module.css";
 import { modelName } from "../data/modelInfo";
+import LanguageSwitcher from "./LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
+    const { t } = useTranslation();
     return (
         <header className={styles.header}>
             <Link to="/" className={styles.logo}>
                 {modelName}
             </Link>
+            <LanguageSwitcher />
             <Link to="/" className={styles.link}>
-                Back to Home
+                {t("galleryHeader.backToHome")}
             </Link>
         </header>
     )
